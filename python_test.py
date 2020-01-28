b7b39653c923650cd64a3217351423eb0b3f1376
wei_don@yahoo.com <wei_don@yahoo.com>
2020-01-27 22:16:16 -0500

    ls working

--- python_test.py
+++ python_test.py
@@ -2,7 +2,7 @@
 import subprocess;
 #stdout="abc"
 #stderr="def";
-out =subprocess.Popen(['wc', '-l','my_text_file.txt'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT);
+out =subprocess.Popen(['ls', '-al'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT);
 stdout,stderr = out.communicate()
 print(stdout);
 print(stderr);
