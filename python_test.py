4fff90664f33f2e4cb606b411cf0fedb784352f5
dunnywei <wei_don@yahoo.com>
2020-01-28 15:59:30 -0500

    update

--- python_test.py
+++ python_test.py
@@ -1,8 +1,9 @@
+#netperf -H 134.117.197.1 -p 80 -l 100
 #testing
 import subprocess;
 #stdout="abc"
 #stderr="def";
-out =subprocess.Popen(['ls', '-al'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT);
+out =subprocess.Popen(['netperf', '-H 134.117.197.1','-p 80','-l 100'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT);
 stdout,stderr = out.communicate()
 print(stdout);
 print(stderr);
